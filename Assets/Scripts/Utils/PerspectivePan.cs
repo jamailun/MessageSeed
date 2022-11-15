ecce20e1ee95e3ee72429e87487813c3f4d5fdb8
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2022-11-15 11:39:22 +0900

    Added some basic bufferization

--- Assets/Scripts/Utils/PerspectivePan.cs
+++ Assets/Scripts/Utils/PerspectivePan.cs
@@ -2,6 +2,9 @@
 
 public class PerspectivePan : MonoBehaviour {
     [SerializeField] private float groundZ = 0;
+    [SerializeField] private float zoomOutMin = 1;
+    [SerializeField] private float zoomOutMax = 8;
+    [SerializeField] private float zoomSensitivity = 0.01f;
 
     private Camera _camera;
     private Vector3 _touchStart;
@@ -14,9 +17,29 @@ public class PerspectivePan : MonoBehaviour {
         if(Input.GetMouseButtonDown(0)) {
             _touchStart = GetWorldPosition(groundZ);
         }
-        if(Input.GetMouseButton(0)) {
+
+        if(Input.touchCount == 2) {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            Zoom(difference * zoomSensitivity);
+        } else if(Input.GetMouseButton(0)) {
             Vector3 direction = _touchStart - GetWorldPosition(groundZ);
             _camera.transform.position += direction;
+        }
+
+        if(Input.GetKey(KeyCode.KeypadPlus)) {
+            Zoom(zoomSensitivity);
+		} else if(Input.GetKey(KeyCode.KeypadMinus)) {
+            Zoom( - zoomSensitivity);
         }
     }
 
@@ -25,5 +48,9 @@ public class PerspectivePan : MonoBehaviour {
         Plane ground = new(Vector3.forward, new Vector3(0, 0, z));
 		ground.Raycast(mousePos, out float distance);
 		return mousePos.GetPoint(distance);
+    }
+
+    private void Zoom(float increment) {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
     }
 }
