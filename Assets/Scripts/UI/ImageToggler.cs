154f65fef2314235dd30ce0dcba8d41864a3b0f9
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2022-12-17 11:00:30 +0900

    Just renamed 'ChangeImage' to 'ImageToggler' to avoid future git problems

--- Assets/Scripts/UI/ImageToggler.cs
+++ Assets/Scripts/UI/ImageToggler.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChangeImage : MonoBehaviour {
+public class ImageToggler : MonoBehaviour {
 
     [SerializeField] private Sprite spriteIcon;
     [SerializeField] private Sprite spriteClosed;
