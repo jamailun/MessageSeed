fbfdd10fa66f8d826453ea8963e98b2f99559364
isabella <isabella.grossart@gmx.de>
2022-12-19 11:54:54 +0100

    Animation for Profile added. Couldn't resolve the button task. Problems with Open Event () and Close Event. The State can't be changed without passing through the Default State.

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -7,25 +7,56 @@ public class MainMenuUI : MonoBehaviour {
 
 	[SerializeField] private MainMenuWindowUI windowSettings;
 	[SerializeField] private MainMenuWindowUI windowNewMessage;
+	[SerializeField] private MainMenuWindowUI windowProfile;
+	[SerializeField] private MainMenuWindowUI windowMessages;
+	[SerializeField] private MainMenuWindowUI windowFertilizer;
+	[SerializeField] private MainMenuWindowUI windowFriends;
 
-	public void TriggerOpenSettings() {
-		TriggerHandleOpen(State.SHOW_SETTINGS, windowSettings);
+	public void TriggerOpenSettings(int x) {
+		if (x == 0)
+			TriggerHandleOpen(State.SHOW_SETTINGS, windowSettings);
+		else if (x == 1)
+			TriggerHandleOpen(State.SHOW_PROFILE, windowProfile);
 	}
 
 	public void TriggerOpenNewMessage() {
 		TriggerHandleOpen(State.SHOW_NEW_MESSAGE, windowNewMessage);
 	}
 
-	public void CloseEverything() {
+    public void TriggerOpenProfile() {
+		TriggerHandleOpen(State.SHOW_PROFILE, windowProfile);
+    }
+
+	public void TriggerOpenMessages() {
+		TriggerHandleOpen(State.SHOW_MESSAGES, windowMessages);
+    }
+
+	public void TriggerOpenFertilizer() {
+		TriggerHandleOpen(State.SHOW_FERTILIZER, windowFertilizer);
+    }
+
+    public void TriggerOpenFriends() {
+		TriggerHandleOpen(State.SHOW_FRIENDS, windowFriends);
+    }
+
+    public void CloseEverything() {
 		windowSettings.Close();
 		windowNewMessage.Close();
+		windowProfile.Close();
+		windowMessages.Close();
+		windowFertilizer.Close();
+		windowFriends.Close();
 		state = State.DEFAULT;
 	}
 
 	private enum State {
 		DEFAULT,
 		SHOW_SETTINGS,
-		SHOW_NEW_MESSAGE
+		SHOW_NEW_MESSAGE,
+		SHOW_PROFILE,
+		SHOW_MESSAGES,
+		SHOW_FERTILIZER,
+		SHOW_FRIENDS
 	}
 
 	private void TriggerHandleOpen(State newState, MainMenuWindowUI window) {
