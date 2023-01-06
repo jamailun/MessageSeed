78ed03dc5764768e2e4a85f99727c9cdcd59f930
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2023-01-06 19:28:06 +0900

    Added a way to open message when click on them

---
+++ Assets/Scripts/Messages/MessageRaycast.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MessageRaycast : MonoBehaviour {
+
+	[SerializeField] private LayerMask layerMask;
+
+	public MessageOpenedEvent messageOpenedEvent;
+
+	private void Update() {
+		bool drag;
+		if(Application.isMobilePlatform) {
+			drag = Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began;
+		} else {
+			drag = Input.GetMouseButton(0) && Input.GetAxisRaw("Mouse X") == 0.0f && Input.GetAxisRaw("Mouse Y") == 0.0f;
+		}
+
+		if(!drag) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) {
+				if(hit.collider.GetComponent<MessageRenderer>() != null) {
+					MessageRenderer renderer = hit.collider.GetComponent<MessageRenderer>();
+					renderer.OpenOrLoad(MessageReadyToOpen);
+				}
+				//Coordinates gpsCoordinates = Coordinates.convertVectorToCoordinates(hit.point);
+			}
+		}
+	}
+
+	private void MessageReadyToOpen(Message message) {
+		messageOpenedEvent?.Invoke(message);
+	}
+
+	[System.Serializable]
+	public class MessageOpenedEvent : UnityEvent<Message> { }
+
+
+}
