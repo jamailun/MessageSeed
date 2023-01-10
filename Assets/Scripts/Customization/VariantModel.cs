39908bfb01018896c1e89a0419bf976b88071454
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2023-01-10 11:59:54 +0900

    Added a way to change the avatar color dynamically

--- Assets/Scripts/Customization/VariantModel.cs
+++ Assets/Scripts/Customization/VariantModel.cs
@@ -3,11 +3,10 @@
 [CreateAssetMenu(fileName = "VariantModel", menuName = "MessageSeed/VariantModel", order = 1)]
 public class VariantModel : ScriptableObject {
 
-	[SerializeField] private string _variantName;
 	[SerializeField] private Color _variantColor;
 	[SerializeField] private Material _variantMaterial;
 
-	public string VariantName => _variantName;
+	public string VariantName => name;
 	public Color VariantColor => _variantColor;
 	public Material VariantMaterial => _variantMaterial;
 
---
+++ Assets/Scripts/Customization/VariantModelUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class VariantModelUI : MonoBehaviour {
+
+	[SerializeField] private VariantModel model;
+
+	private void Awake() {
+		GetComponent<Image>().color = model.VariantColor;
+	}
+
+	private void Start() {
+		
+	}
+
+	public void LocalClick() {
+		Debug.Log("new color : " + model.name);
+		DynamicAvatar.Instance?.ChangeColor(model);
+	}
+
+}
