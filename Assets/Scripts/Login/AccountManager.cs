7b7ea5e98ed51681ccc11b5456785e61c8ab663f
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2023-01-12 17:56:02 +0900

    Created basic elementsfor sign-in

--- Assets/Scripts/Login/AccountManager.cs
+++ Assets/Scripts/Login/AccountManager.cs
@@ -46,12 +46,22 @@ public class AccountManager : MonoBehaviour {
 	}
 
 
-	public void TryLogin(string username, string password, CSharpExtension.Consumable<string> errorCallback = null, CSharpExtension.Runnable successCallback = null) {
+	public void TryLogin(string username, string password, CSharpExtension.Consumable<string> errorCallback = null) {
 		if(IsLogged) {
 			Debug.LogWarning("Tried to log-in... But you're already logged-in !");
+			errorCallback?.Invoke("Cannot log-in : already logged-in !");
 			return;
 		}
-		StartCoroutine(CR_SendLogin(username, password, errorCallback, successCallback));
+		StartCoroutine(CR_SendLogin(username, password, errorCallback));
+	}
+
+	public void TrySignIn(string username, string mail, string password, CSharpExtension.Consumable<string> errorCallback = null) {
+		if(IsLogged) {
+			Debug.LogWarning("Tried to sign-in... But you're already logged-in !");
+			errorCallback?.Invoke("Cannot sign-in : already logged-in !");
+			return;
+		}
+		StartCoroutine(CR_SendSignIn(username, mail, password, errorCallback));
 	}
 
 	public void TryLogout() {
@@ -71,7 +81,7 @@ public class AccountManager : MonoBehaviour {
 		SceneManager.LoadScene(mainSceneName);
 	}
 
-	private IEnumerator CR_SendLogin(string user, string password, CSharpExtension.Consumable<string> errorCallback, CSharpExtension.Runnable successCallback) {
+	private IEnumerator CR_SendLogin(string user, string password, CSharpExtension.Consumable<string> errorCallback) {
 		string postData = JsonUtility.ToJson(new LoginRequest(user, password));
 		byte[] postDataRaw = System.Text.Encoding.UTF8.GetBytes(postData);
 
@@ -83,12 +93,29 @@ public class AccountManager : MonoBehaviour {
 				errorCallback?.Invoke(www.error + " : " + www.downloadHandler?.text);
 			} else {
 				var data = JsonUtility.FromJson<LoginResponse>(www.downloadHandler.text);
-				SetLoginComplete(data, user);
 				Debug.Log("LOGIN COMPLETED !");
-				successCallback?.Invoke();
+				SetLoginComplete(data, user);
 			}
 		}
 	}
+
+	private IEnumerator CR_SendSignIn(string user, string mail, string password, CSharpExtension.Consumable<string> errorCallback) {
+		string postData = JsonUtility.ToJson(new SignInRequest(user, mail, password));
+		byte[] postDataRaw = System.Text.Encoding.UTF8.GetBytes(postData);
+		using(UnityWebRequest www = RemoteApiManager.Instance.CreatePostRequest("/api_auth/login/", postDataRaw)) {
+			yield return www.SendWebRequest();
+
+			if(www.result != UnityWebRequest.Result.Success) {
+				Debug.LogError(www.error + " : " + www.downloadHandler?.text);
+				errorCallback?.Invoke(www.error + " : " + www.downloadHandler?.text);
+			} else {
+				var data = JsonUtility.FromJson<SignInResponse>(www.downloadHandler.text);
+				Debug.Log("SIGNGIN COMPLETED ! Now sending log-in...");
+				StartCoroutine(CR_SendLogin(user, password, errorCallback));
+			}
+		}
+	}
+
 	private IEnumerator CR_SendLogout() {
 		string postData = JsonUtility.ToJson(new LogoutRequest(TokenRefresh));
 		byte[] postDataRaw = System.Text.Encoding.UTF8.GetBytes(postData);
@@ -114,6 +141,13 @@ internal struct LoginResponse {
 }
 
 [System.Serializable]
+internal struct SignInResponse {
+	public string username;
+	public string email;
+}
+
+
+[System.Serializable]
 internal struct LoginRequest {
 	public string username;
 	public string password;
@@ -122,6 +156,25 @@ internal struct LoginRequest {
 		password = p;
 	}
 }
+
+[System.Serializable]
+internal struct SignInRequest {
+	public string username;
+	public string password;
+	public string password2;
+	public string email;
+	public string first_name;
+	public string last_name;
+	internal SignInRequest(string u, string m, string p) {
+		username = u;
+		password = p;
+		password2 = p;
+		email = m;
+		first_name = u;
+		last_name = "-";
+	}
+}
+
 [System.Serializable]
 internal struct LogoutRequest {
 	public string refresh_token;
