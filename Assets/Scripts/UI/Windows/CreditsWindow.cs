01409acb2eb4b218accf72ad058984fc324e37bd
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2023-01-16 17:25:09 +0900

    Added the credits page

---
+++ Assets/Scripts/UI/Windows/CreditsWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CreditsWindow : MainMenuWindowUI {
+
+	[Header("URL config")]
+	[SerializeField] private string clientUrl = "https://github.com/jamailun/MessageSeed";
+	[SerializeField] private string serverUrl = "https://github.com/JackyL56/MessageSeed_Django";
+
+	public void Button_Client() {
+		Application.OpenURL(clientUrl);
+	}
+	public void Button_Server() {
+		Application.OpenURL(serverUrl);
+	}
+
+}
